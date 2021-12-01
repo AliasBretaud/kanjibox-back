ef5b37f3ab22a01d3a414bc2907bbf769422e528
AliasBretaud <lcdopg@gmail.com>
2021-12-01 11:25:00 +0100

    [EVOL] Switch to postgres

--- src/main/java/flo/no/kanji/entity/KanjiEntity.java
+++ src/main/java/flo/no/kanji/entity/KanjiEntity.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 public class KanjiEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ElementCollection
