dd0f7bbb5dc13ed1c92f623532a82c505e180f07
Florian Bretaud <lcdopg@gmail.com>
2025-05-15 12:51:02 +0900

    fix sql

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -1,7 +1,12 @@
 -- Clean all tables data
-TRUNCATE kanji CASCADE;
-TRUNCATE word CASCADE;
-TRUNCATE users CASCADE;
+DELETE FROM word_translation;
+DELETE FROM word_kanji;
+DELETE FROM word;
+DELETE FROM kanji_translation;
+DELETE FROM kanji_kun_yomi;
+DELETE FROM kanji_on_yomi;
+DELETE FROM kanji;
+DELETE FROM users;
 
 --
 -- TOC entry 3334 (class 0 OID 16556)
