6dc451aa289cca91eb5e11d7ed15fb293c02c3e4
Florian Bretaud <lcdopg@gmail.com>
2026-04-05 19:47:08 +0900

    fix: set default value for deepl key

--- src/main/java/flo/no/kanji/conf/DeeplTranslatorConfig.java
+++ src/main/java/flo/no/kanji/conf/DeeplTranslatorConfig.java
@@ -1,6 +1,7 @@
 package flo.no.kanji.conf;
 
 import com.deepl.api.DeepLClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,6 @@ public class DeeplTranslatorConfig {
 
     @Bean
     public DeepLClient deeplClient() {
-        return new DeepLClient(deeplApiKey);
+        return StringUtils.isBlank(deeplApiKey) ? null : new DeepLClient(deeplApiKey);
     }
 }
