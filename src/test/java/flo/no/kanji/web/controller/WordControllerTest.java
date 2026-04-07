fac458c0b578d083c8060a60a00cae4f26cea0d1
Florian Bretaud <lcdopg@gmail.com>
2026-04-07 22:40:47 +0900

    fix: unit tests

--- src/test/java/flo/no/kanji/web/controller/WordControllerTest.java
+++ src/test/java/flo/no/kanji/web/controller/WordControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "kanji.translation.auto.enable=true")
 @Slf4j
 public class WordControllerTest {
 
